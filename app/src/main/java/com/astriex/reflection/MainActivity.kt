1ddf3f0440479ce768a5489d9beb6cd0304524a1
Astriex <melishuskanovic@hotmail.com>
2021-04-13 20:56:21 +0200

    Setup login and sign up screens

--- app/src/main/java/com/astriex/reflection/MainActivity.kt
+++ app/src/main/java/com/astriex/reflection/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.astriex.reflection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.astriex.reflection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        bindingMain.btnStart.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
