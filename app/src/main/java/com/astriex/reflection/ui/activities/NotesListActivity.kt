e937febb1c7891f453e5183d615dc52ead4eabe2
Astriex <melishuskanovic@hotmail.com>
2021-04-17 10:34:20 +0200

    menu added to NotesListActivity

--- app/src/main/java/com/astriex/reflection/ui/activities/NotesListActivity.kt
+++ app/src/main/java/com/astriex/reflection/ui/activities/NotesListActivity.kt
@@ -1,12 +1,45 @@
 package com.astriex.reflection.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.astriex.reflection.R
+import com.astriex.reflection.databinding.ActivityNotesListBinding
 
 class NotesListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNotesListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notes_list)
+        binding = ActivityNotesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.actionAdd -> {
+                startNewNote()
+            }
+            R.id.actionSignOut -> {
+                logout()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun startNewNote() {
+        startActivity(Intent(this, PostNoteActivity::class.java))
+        finish()
+    }
+
+    private fun logout() {
+
     }
 }
